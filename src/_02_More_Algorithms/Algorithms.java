0d6e18a567cd57c5454898d305ca53133f95d0dc
Sean Yoo <31218878+pigchick3n@users.noreply.github.com>
2020-03-17 20:01:33 -0700

    corona

--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -4,6 +4,43 @@ import java.util.List;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
+		for (int i = 0; i < eggs.size(); i++) {
+			if (eggs.get(i) == "cracked") {
+				return i;
+			}
+		}
 		return 0;
+	
+}
+	public static int countPearls(List<Boolean> oysters) {
+		int counter = 0;
+		for (int i = 0; i < oysters.size(); i++) {
+			if (oysters.get(i)) {
+				counter++;
+			}
+		}
+	return counter;
 	}
+	public static double findTallest(List<Double> peeps) {
+		double height = 0;
+		for (int i = 0; i < peeps.size()-1; i++) {
+			if (peeps.get(i+1) >= peeps.get(i)) {
+				height = peeps.get(i+1);
+			}
+		}
+		return height;
+	
+}
+	public static String findLongestWord(List<String> words) {
+		String longest = "";
+		int characters = 0;
+		for (int i = 0; i < words.size(); i++) {
+			if (words.get(i).length() >= characters) {
+				characters = words.get(i).length();
+				longest = words.get(i);
+			}
+		}
+		return longest;
+	
+}
 }
